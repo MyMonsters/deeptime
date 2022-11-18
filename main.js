44d84c87c32c5048eca465bd2c80e82af9389eec
MyMonsters <62538105+MyMonsters@users.noreply.github.com>
2022-11-18 13:12:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+    // $(document).ready(function(){ 
+    //   $("#mainNav li a").each(function(){ 
+        
+    //     if($(this)[0].href==String(window.location)){ 
+    //       $(this).addClass("active").siblings().removeClass("active"); 
+    //       //$(this).parent('li').addClass("on").siblings().removeClass("on"); 直接父级
+    //       console.log('ddd')
+    //     } 
+    //   }); 
+    // }); 
+  //   window.onscroll = function() {
+  //     var nav = document.getElementById('mainNav');
+      
+  //     if(window.pageYOffset >= 70){
+        
+  //       nav.classList.remove('navigator-bg');
+  //       nav.classList.add('bg-white')
+  //     } else {
+  //       nav.classList.add('navigator-bg');
+  //       nav.classList.remove('bg-white')
+  //     }
+  
+  //  }
+  
+
